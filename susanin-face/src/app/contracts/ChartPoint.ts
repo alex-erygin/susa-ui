5127016bb817292e60cf360e498b43272c5b2818
alex-erygin <alex.erygin@gmail.com>
2021-01-08 23:58:26 +0500

    Hide multiple columns, show today date

--- susanin-face/src/app/contracts/ChartPoint.ts
+++ susanin-face/src/app/contracts/ChartPoint.ts
@@ -1,11 +1,18 @@
+import { DatePipe } from '@angular/common'
+
 export default class ChartPoint {
   constructor(date: Date, name: string, value: number) {
     this.date = date ?? null
     this.name = name
     this.value = value
+
+    const options = {  year: 'numeric', month: 'numeric', day: 'numeric' }
+
+    this.point_label = this.date.toLocaleDateString("ru-RU", options)
   }
 
   date?: Date
   name: string
   value: number
+  point_label: string
 }
